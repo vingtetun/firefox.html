97b9932b8d8c2f5817191b4cf41fe77f443725ec
Vivien Nicolas <vnicolas@mozilla.com>
2015-01-07 01:54:15 +0100

    Remove some useless licence declaration at the beginning of files

--- shared/js/urlhelper.js
+++ shared/js/urlhelper.js
@@ -1,7 +1,3 @@
-/* this source code form is subject to the terms of the mozilla public
- * license, v. 2.0. if a copy of the mpl was not distributed with this
- * file, you can obtain one at http://mozilla.org/mpl/2.0/. */
-
 /**
  * urlhelper.js
  */
