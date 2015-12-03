04a865b77e109d2a0f7308a443ee3a6b55748ea9
Vivien Nicolas <vnicolas@mozilla.com>
2015-12-03 17:40:11 +0100

    Disable the nofocus for now as it breaks the world...

--- panels/tabs/js/nofocus.js
+++ panels/tabs/js/nofocus.js
@@ -2,6 +2,6 @@
 require([], function() {
   // Ensure the panel can not get user focus from mouse.
   window.addEventListener('mousedown', function(e) {
-    e.preventDefault();
+    //e.preventDefault();
   });
 });
