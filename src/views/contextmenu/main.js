b4eb5acebe6c51284ee2aca104dd173617ac8410
Vivien Nicolas <vnicolas@mozilla.com>
2015-12-13 11:24:44 +0100

    For fun, support some contextmenu actions

--- src/views/contextmenu/main.js
+++ src/views/contextmenu/main.js
@@ -1,10 +1,60 @@
+
+var targetURI = null;
+var documentURI = null;
+
 window.addEventListener('message', function(e) {
   var data = e.data;
+
+  function dumpObject(obj, level = 0) {
+    var indent = '';
+    for (var i = 0; i < level; i++) {
+      indent += '\t';
+    }
+
+    for (var key in obj) {
+      if (typeof obj[key] == 'object') {
+        dump(indent + key + '\n');
+        dumpObject(obj[key], level + 1);
+      } else {
+        dump(indent + key + ': ' + obj[key] + '\n');
+      }
+    }
+  }
+
+  //dumpObject(data);
 
   var element = document.querySelector('.contextmenu');
   element.style.left = data.clientX + 'px';
   element.style.top = (data.clientY + 39) + 'px';
   element.style.visibility = 'visible';
+
+  targetURI = data.systemTargets[0].data.uri;
+  documentURI = data.systemTargets[0].data.documentURI;
+});
+
+addEventListener('click', function(e) {
+  var bc = new BroadcastChannel('tabs');
+
+  dump('click: ' + e.target + ':' + e.target.id + '\n');
+  switch (e.target.id) {
+    case 'open-in-new-tab':
+      bc.postMessage({
+        action: 'create',
+        options: {
+          url: targetURI
+        }
+      });
+      break;
+
+    case 'view-page-source':
+      bc.postMessage({
+        action: 'view-source'
+      });
+      break;
+
+    default:
+      break;
+  }
 });
 
 addEventListener('load', function(e) {
